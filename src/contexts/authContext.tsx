26944e94ba0b11ec9759ac3ab8a07d80d7c2a57e
unknown <matheusfariasdasneves@gmail.com>
2024-01-19 22:19:30 -0300

    feat: Adicionar funcionalidade sigInPhysicalPerson que faz requisicao para api na rota de logar um usuario pessoa fisica, salvando os dados em uma useState e salvando o token para autenticacao.

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,18 +1,29 @@
-import { createContext, ReactNode } from 'react';
-import { destroyCookie } from 'nookies';
+import { createContext, ReactNode, useState } from 'react';
+import { destroyCookie, setCookie } from 'nookies';
 import Router from 'next/router';
 
+import { api } from '@/services/apiClient';
+
 interface AuthContextData {
-  sigInPhysicalPerson: (credencials: sigInPhysicalPersonProps) => Promise<void>,
+  sigInPhysicalPerson: (credencials: SigInPhysicalPersonProps) => Promise<void>,
+  signedPhysicalPersonUser: boolean,
 };
 
 interface AuthContextProps{
   children: ReactNode,
 };
 
-interface sigInPhysicalPersonProps{
+interface SigInPhysicalPersonProps{
   email: string,
   password: string,
+};
+
+interface PhysicalPersonProps{
+  id: string,
+  name: string,
+  email: string,
+  contact: string,
+  avatar: string,
 };
 
 export const AuthContext = createContext({} as AuthContextData);
@@ -30,13 +41,42 @@ export function signOut(){
 };
 
 export default function AuthContextProvider({ children }: AuthContextProps){
-  async function sigInPhysicalPerson({ email, password }: sigInPhysicalPersonProps){
-    console.log(email);
-    console.log(password);
+  const [physicalPersonUser, setPhysicalPersonUser] = useState<PhysicalPersonProps>();
+  const signedPhysicalPersonUser = !!physicalPersonUser;
+
+  async function sigInPhysicalPerson({ email, password }: SigInPhysicalPersonProps){
+    try{
+      const response = await api.post('/session', {
+        email,
+        password,
+      });
+
+      const { id, name, contact, avatar, token } = response.data;
+
+      setCookie(undefined, '@dentalsupport.token', token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: '/',
+      });
+
+      setPhysicalPersonUser({
+        id,
+        name,
+        email,
+        contact,
+        avatar,
+      });
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+      Router.push('/dashboarduser');
+
+    }catch(err){
+      console.log('Erro ao tentar fazer login');
+    };
   };
 
   return(
-    <AuthContext.Provider value={{ sigInPhysicalPerson }} >
+    <AuthContext.Provider value={{ sigInPhysicalPerson, signedPhysicalPersonUser }} >
       { children }
     </AuthContext.Provider>
   );
---
+++ src/pages/dashboarduser/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function DashboardUser(){
+  return(
+    <>
+      <Head>
+        <title>DentalSupport - Minhas Consultas</title>
+      </Head>
+      <div>
+        <h1>Página DashboardUser</h1>
+      </div>
+    </>
+  );
+};
