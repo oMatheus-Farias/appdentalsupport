9bc47855bdc8285801d729d4d32c09b5f6dfc983
unknown <matheusfariasdasneves@gmail.com>
2024-01-20 21:28:57 -0300

    feat: Adicionar componentes HeaderMobile, NavigationMenu e NavigationMobile na pagina de dashboard da pessoa juridica.

--- src/pages/dashboardclinic/index.tsx
+++ src/pages/dashboardclinic/index.tsx
@@ -1,16 +1,32 @@
+import { useContext, useState } from 'react';
+import { ScreenSizeContext } from '@/contexts/screenSizeContext';
+
 import Head from 'next/head';
+import { HeaderMobile } from '@/components/headerMobile';
+import { NavigationMenu } from '@/components/navigationMenu';
+import { NavigationMobile } from '@/components/navigationMobile';
 
 import { canSSRAuthLegalPerson } from '@/utils/canSSRAuthLegalPerson';
 
 export default function DashboardClinic(){
+  const { dasktopSizeScreen } = useContext(ScreenSizeContext);
+
+  const [openNav, setOpenNav] = useState(false);
+
   return(
     <>
       <Head>
         <title>DentalSupport - Minha Agenda</title>
       </Head>
-      <div>
-        <h1>Página Dashboard Clinic</h1>
+      <div className='lg:flex' >
+        {dasktopSizeScreen ? <NavigationMenu /> : <HeaderMobile handleOpenNav={ () => setOpenNav(true) } />}
+
+        <main className='p-5' >
+          <h1>Página DashboardClinic</h1>
+        </main>
       </div>
+
+      {!dasktopSizeScreen && <NavigationMobile handleCloseNav={ () => setOpenNav(false) } openNav={ openNav } />}
     </>
   );
 };
