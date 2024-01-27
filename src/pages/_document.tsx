c1ef14752e0cf904f9fe00356038e5e917f8ea3e
unknown <matheusfariasdasneves@gmail.com>
2024-01-26 21:56:30 -0300

    feat: Adicionar meta tags.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head>
         <meta name='author' content='Matheus Farias' />
+        <meta name='description' content='O Dental Support é uma plataforma digital dedicada aos serviços odontológicos, proporcionando uma abordagem abrangente tanto para profissionais de saúde bucal quanto para pacientes. Com uma interface intuitiva e funcionalidades específicas para ambos os públicos, a plataforma visa simplificar o agendamento de consultas, melhorar a gestão de clínicas odontológicas e oferecer uma experiência aprimorada aos usuários.' />
+        <meta name='keyboards' content='Odontologia, Consultório Odontológico, Agendamento de Consultas, Plataforma Odontológica, Saúde Bucal, Marcar Consulta Online, Teleconsulta Odontológica, Paciente, Profissional de Saúde Bucal, Prontuário Eletrônico Odontológico, Agendamento de Serviços Odontológicos, Sistema de Gestão para Consultórios, Perfil do Paciente, Perfil Profissional Odontológico, Banco de Dados Odontológico, Front-End Odontológico, API Odontológica, Next.js, React.js, TailwindCSS, Figma, Prisma ORM, Node.js, PostgreSQL, Telemedicina Odontológica' />
       </Head>
       <body>
         <Main />
